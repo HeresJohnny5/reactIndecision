bd829254d37504cd5ae181d1fd04ee77af9b99c1
John Erickson <ericksonjohn08@hotmail.com>
2018-09-23 15:29:57 -0400

    Add .gitignore file. Create package.json file with 2 babel dependencies. Remove code from app.js in scripts directory to app.js in src directory.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-'use strict';
-
 console.log('App.js is running!');
 
 // var template = <p>This is JSX from app.js!</p>;
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var template = <h1>Indecision App</h1>
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
