d785ec085a3acd230fd07379e5ddfc27316b1e54
John Erickson <ericksonjohn08@hotmail.com>
2018-10-05 15:32:43 -0400

    Add directory structure for scss. Add use array for multiple scss rules. Add normalize.css.

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,21 +3,19 @@ import React from 'react';
 // LOCAL IMPORTS
 import Option from './Option';
 
-const Options = (props) => {
-  return (
-    <div>
-      {props.options.length === 0 && <p>Please add an option to get started!</p>}
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.map(option => (
-          <Option 
-            key={option} 
-            optionText={option} 
-            handleDeleteOption={props.handleDeleteOption} 
-          />
-        ))
-      }
-    </div>
-  );
-};
+const Options = (props) => (
+  <div>
+    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.map(option => (
+        <Option 
+          key={option} 
+          optionText={option} 
+          handleDeleteOption={props.handleDeleteOption} 
+        />
+      ))
+    }
+  </div>
+);
 
 export default Options;
