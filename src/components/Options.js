f152d49511bcb260cbb1425f492f11aac814633e
John Erickson <ericksonjohn08@hotmail.com>
2018-10-06 09:43:22 -0400

    Create additional styles.

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,21 +5,25 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button 
-      className="button button--link" 
-      onClick={props.handleDeleteOptions}
-    >
-      Remove All
-    </button>
-    {props.options.map(option => (
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button 
+        className="button button--link" 
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
+    </div>
+    {props.options.map((option, index) => (
       <Option 
+        index={index + 1}
         key={option} 
         optionText={option} 
         handleDeleteOption={props.handleDeleteOption} 
       />
     ))
     }
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
   </div>
 );
 
